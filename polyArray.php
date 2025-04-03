eebe8dd6f9ffa97603b3d73cdf35a1eea1cc5ed8
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2025-04-03 15:05:45 +0300

    Add files via upload

--- polyArray.php
+++ polyArray.php
@@ -4,7 +4,13 @@ $cars = array(
     array("Ford", 23, 19),
     array("Volvo", 21, 17)
 );
-
+echo "<br>" . "First array" . "<br>";
 foreach ($cars as $car) {
-    echo $car[0] . " " .$car[1] . " " . $car[2] . "<br>";
+    echo "<br>" . $car[0] . " " .$car[1] . " " . $car[2] . "<br>";
+}
+array_splice($cars, 1,1 );
+
+echo "<br>" . "Second array" . "<br>";
+foreach ($cars as $car) {
+    echo "<br>" . $car[0] . " " .$car[1] . " " . $car[2] . "<br>";
 }
